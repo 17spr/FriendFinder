6b0194fa121dde644ad428249f3e7e805f998f6b
jesse <jessemspringer@gmail.com>
2018-03-17 21:59:30 -0400

    Error: Cannot find module './routing/htmlRoutes.js

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-// Dependencies 
+// dependencies 
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 
-
 var app = express();
-var PORT = 8080;
+
+// 
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// require("./routing/htmlRoutes.js")(app);
-// require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+require("./routing/apiRoutes.js")(app);
+
 
 app.listen(PORT, function() {
-    console.log("App listening on http://localhost:" + PORT);
-  })
+  console.log("App listening on PORT " + PORT);
+})
+
