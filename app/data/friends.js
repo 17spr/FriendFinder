ece126d11a7ac787cbcf239380be81222294031a
jesse <jessemspringer@gmail.com>
2018-03-22 20:29:41 -0400

    README.md edits

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,115 @@
-var friends = [
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      }
-];
+var friends = [{
+    "name": "The Grinch",
+    "photo": 'http://www.indiewire.com/wp-content/uploads/2018/02/shutterstock_5875878f.jpg?w=780',
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Russell Brand",
+    "photo": 'https://res.cloudinary.com/jpress/image/fetch/ar_3:2,c_fill,f_auto,h_679,q_auto:eco,w_960/https://inews.co.uk/wp-content/uploads/2017/09/GettyImages-149442474-960x679.jpg',
+    "scores": [
+        2,
+        4,
+        5,
+        2,
+        3,
+        1,
+        3,
+        5,
+        1,
+        4
+    ]
+},
+{
+    "name": "Michelle Obama",
+    "photo": 'https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/11/16/104112634-GettyImages-577604280.1910x1000.jpg',
+    "scores": [
+        5,
+        5,
+        5,
+        1,
+        1,
+        1,
+        3,
+        2,
+        3,
+        2
+    ]
+},
+{
+    "name": "The Queen of England",
+    "photo": 'https://www.mercurynews.com/wp-content/uploads/2017/04/aptopix-britain-queens-bir.jpg?w=523',
+    "scores": [
+        2,
+        2,
+        2,
+        3,
+        4,
+        3,
+        4,
+        5,
+        1,
+        1
+    ]
+},
+{
+    "name": "Ozzy Osbourne",
+    "photo": 'https://cdn.movieweb.com/img.news.tops/NExEdZzPQN5SBF_1_b/Ozzy-Osbourne-Farewell-Tour-2020.jpg',
+    "scores": [
+        1,
+        1,
+        4,
+        4,
+        2,
+        2,
+        4,
+        3,
+        3,
+        3
+    ]
+},
+{
+    "name": "David Gray",
+    "photo": 'http://www.davidgray.com/wp-content/uploads/2017/12/dgchristmas-5.jpg',
+    "scores": [
+        1,
+        1,
+        1,
+        5,
+        5,
+        5,
+        3,
+        3,
+        2,
+        2
+    ]
+},
+{
+    "name": "Michael Jackson",
+    "photo": 'https://pmcvariety.files.wordpress.com/2017/06/michael-jackson.jpg?w=1000&h=563&crop=1',
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        1,
+        1,
+        1,
+        3,
+        2
+    ]
+}
+]
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,10 +4,66 @@ var friends = require('../data/friends');
 
 
 module.exports = function (app) {
-
+    // GET method (for getting array of friends in friends.js file)
     app.get("/api/friends", function(req, res) {
         res.json(friends);
         console.log(friends)
     });
+    // POST method (for new user survey responses)
+    app.post("/api/friends", function(req, res) {
 
-};
+        var userSurveyAnswers = req.body;
+
+        userSurveyAnswers.name = userSurveyAnswers.name.replace(/\s+/g, "");
+
+        userSurveyAnswers.photo = userSurveyAnswers.photo.replace(/\s+/g, "");
+
+        convertStringToInt(userSurveyAnswers.scores);
+
+       
+        var userScore = userSurveyAnswers.scores;
+        // variables for storing 'new friend' name and image
+        var matchName = '';
+        var matchImage = '';
+
+        // just a number to start off with
+        var totalDifference = 10000;
+
+        // Logic for comparing friend and newUser scores to find the 'new best friend'
+        for (var i = 0; i < friends.length; i++) {
+            var diff = 0;
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                // determining the absolute value of friends score and new user's score
+                // then subtracting the user score from the friend score and updating the variable called `diff` to store the new difference
+                diff += Math.abs(friends[i].scores[j] - userScore[j]);
+            }
+            if (diff < totalDifference) {
+                totalDifference = diff;
+                // creating a new best friend...
+                matchName = friends[i].name;
+                matchImage = friends[i].photo;
+            }
+        }
+
+        // Add user's inputs into the friends array
+        friends.push(userSurveyAnswers);
+
+        // Respond with json of name and photo of the matched friend
+        res.json({
+            status: 'OK',
+            matchName: matchName,
+            matchImage: matchImage
+        });
+
+
+    });
+
+}
+
+
+function convertStringToInt(userSurveyAnswers) {
+    for (var i = 0; i < userSurveyAnswers.length; i++) {
+        userSurveyAnswers[i] = parseInt(userSurveyAnswers[i]);
+    }
+}
+
